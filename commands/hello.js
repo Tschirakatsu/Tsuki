ea0c00ed24d61bfbd433a8465e107f627764bf58
Tschirakatsu <tschirakatsu@gmail.com>
2024-06-01 19:27:44 +0200

    update gitignore and hello and index.js implementations

---
+++ commands/hello.js
@@ -0,0 +1,41 @@
+/**
+ * @file   hello.js
+ * @brief  This command is used to say hello to someone and specify a reason
+ * @author Created by Tschirakatsu
+ * @version 26.9.2023
+ */
+const { CommandInteraction } = require("discord.js");
+
+
+module.exports = {
+    data: {
+        name: "hello",
+        description: "say hello to someone",
+        options: [
+            {
+                name: "user",
+                description: "say hello to who ?",
+                type: 6,
+                required: true,
+            },
+            {
+                name: "reason",
+                description: "but why ?",
+                type: 3,
+                required: true,
+            },
+        ],
+    },
+    async execute(interaction) {
+        const { member, options } = interaction;
+        const userOption = options.getMember("user");
+        const reasonOption = interaction.options.getString('reason');
+        const user = interaction.user;
+
+        // Mentionner l'utilisateur dans la réponse à l'interaction
+        interaction.reply({
+            content: `${user} is saying hello to ${userOption.toString()} because ${reasonOption}`,
+            ephemeral: false,
+        });
+    },
+};
---
+++ index.js
@@ -0,0 +1,61 @@
+// index.js
+const { Client, GatewayIntentBits, Collection } = require("discord.js");
+require('dotenv').config();
+const fs = require("fs");
+const {token} = require("./Credentials");
+const client = new Client({
+    intents: [
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.GuildMessages,
+        GatewayIntentBits.MessageContent
+    ]
+});
+
+// Creating a collection to stock the commands
+client.commands = new Collection();
+
+// charge commands from Commands folder
+const commandFiles = fs.readdirSync("./commands").filter(file => file.endsWith(".js"));
+
+for (const file of commandFiles) {
+    const command = require(`./commands/${file}`);
+    // add every command to the collection
+    client.commands.set(command.data.name, command);
+}
+
+client.on("ready", async () => {
+    console.log("Bot Opérationnel");
+
+    // save slash commands
+    const commands = await client.application?.commands.set(Array.from(client.commands.values()).map(command => command.data));
+
+    console.log(`Commandes slash enregistrées : ${commands.map(command => command.name).join(", ")}`);
+});
+
+client.on("interactionCreate", async (interaction) => {
+    if (!interaction.isCommand()) {
+        return;
+    }
+
+    const { commandName } = interaction;
+
+    // verify if the command exists into the collection
+    const command = client.commands.get(commandName);
+
+    if (!command) {
+        return;
+    }
+
+    try {
+        // execute the command
+        await command.execute(interaction);
+    } catch (error) {
+        console.error(error);
+        interaction.reply({
+            content: "Une erreur s'est produite lors de l'exécution de cette commande.",
+            ephemeral: true,
+        });
+    }
+});
+
+client.login(token);
