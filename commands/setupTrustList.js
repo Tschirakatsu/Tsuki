345217fc336779e15be917bf6d74ac87344b53b7
tschirakatsu <tschirakatsu@gmail.com>
2024-06-23 20:44:03 +0200

    Add error handling allow more flexibility on the files path

--- commands/setupTrustList.js
+++ commands/setupTrustList.js
@@ -1,5 +1,6 @@
 const { SlashCommandBuilder, Permissions, MessageEmbed } = require('discord.js');
 const fs = require('fs');
+const { join } = require('path');
 
 module.exports = {
     data: new SlashCommandBuilder()
@@ -9,59 +10,74 @@ module.exports = {
             option
                 .setName('allowed_roles')
                 .setDescription('Roles allowed to send messages and use commands in the trustlist channel')
-                .setRequired(true)),
+                .setRequired(true)
+        ),
     async execute(interaction) {
-        const guildId = interaction.guild.id;
-        const jsonFile = `./Trustlists/Servers/${guildId}.json`; // Generate a JSON file with ServerID as filename
+        try {
+            const guildId = interaction.guild.id;
+            const trustlistsPath = join(__dirname, 'Trustlists', 'Servers');
+            if (!fs.existsSync(trustlistsPath)) {
+                fs.mkdirSync(trustlistsPath);
+            }
+            const jsonFile = `${trustlistsPath}/${guildId}.json`; // Generate a JSON file with ServerID as filename
 
-        // Create or fetch the roles
-        const trustedRole = await getOrCreateRole(interaction.guild, 'Trusted', '#00ff00');
-        const untrustedRole = await getOrCreateRole(interaction.guild, 'Untrusted', '#ff0000');
-        const memberRole = await getOrCreateRole(interaction.guild, 'Member', '#0e70bb');
+            // Create or fetch the roles
+            const trustedRole = await getOrCreateRole(interaction.guild, 'Trusted', '#00ff00');
+            const untrustedRole = await getOrCreateRole(interaction.guild, 'Untrusted', '#ff0000');
+            const memberRole = await getOrCreateRole(interaction.guild, 'Member', '#0e70bb');
 
-        // Store the role IDs in a JSON file
-        const data = {
-            trustedRoleId: trustedRole.id,
-            untrustedRoleId: untrustedRole.id,
-            memberRoleId: memberRole.id,
-        };
-        fs.writeFileSync(jsonFile, JSON.stringify(data));
+            // Store the role IDs in a JSON file
+            const data = {
+                trustedRoleId: trustedRole.id,
+                untrustedRoleId: untrustedRole.id,
+                memberRoleId: memberRole.id,
+            };
+            fs.writeFileSync(jsonFile, JSON.stringify(data));
 
-        // Get the allowed roles from the interaction options
-        const allowedRoles = interaction.options.get('allowed_roles').map(role => role.id);
-        // Create a channel named "TrustList" and send an embed
-        const trustlistChannel = await interaction.guild.channels.create('TrustList', {
-            type: 'GUILD_TEXT',
-            permissionOverwrites: [
-                {
-                    id: interaction.guild.id,
-                    deny: [Permissions.FLAGS.SEND_MESSAGES, Permissions.FLAGS.USE_APPLICATION_COMMANDS],
-                },
-                ...allowedRoles.map((roleId) => ({
-                    id: roleId,
-                    allow: [Permissions.FLAGS.SEND_MESSAGES, Permissions.FLAGS.USE_APPLICATION_COMMANDS, Permissions.FLAGS.MANAGE_ROLES],
-                })),
-            ],
-        });
+            // Get the allowed roles from the interaction options
+            const allowedRoles = interaction.options.get('allowed_roles').map((role) => role.id);
+            // Create a channel named "TrustList" and send an embed
+            const trustlistChannel = await interaction.guild.channels.create('TrustList', {
+                type: 'GUILD_TEXT',
+                permissionOverwrites: [
+                    {
+                        id: interaction.guild.id,
+                        deny: [Permissions.FLAGS.SEND_MESSAGES, Permissions.FLAGS.USE_APPLICATION_COMMANDS],
+                    },
+                    ...allowedRoles.map((roleId) => ({
+                        id: roleId,
+                        allow: [Permissions.FLAGS.SEND_MESSAGES, Permissions.FLAGS.USE_APPLICATION_COMMANDS, Permissions.FLAGS.MANAGE_ROLES],
+                    })),
+                ],
+            });
 
-        // Create an embed for the trustlist setup
-        const embed = new MessageEmbed()
-            .setTitle('TrustList Setup')
-            .setDescription(`This channel has been set up as the trustlist for the server '${interaction.guild.name}'`)
-            .setColor('#0099ff'); // Blue color for embed
+            // Create an embed for the trustlist setup
+            const embed = new MessageEmbed()
+                .setTitle('TrustList Setup')
+                .setDescription(`This channel has been set up as the trustlist for the server '${interaction.guild.name}'`)
+                .setColor('#0099ff'); // Blue color for embed
 
-        await trustlistChannel.send({ embeds: [embed] });
+            await trustlistChannel.send({ embeds: [embed] });
+        } catch (error) {
+            console.error(error);
+            await interaction.reply('Error setting up trustlist. Please try again later.');
+        }
     },
 };
 
 async function getOrCreateRole(guild, roleName, color) {
-    const existingRole = guild.roles.cache.find((role) => role.name === roleName);
-    if (existingRole) {
-        return existingRole;
-    } else {
-        return await guild.roles.create({
-            name: roleName,
-            color: color,
-        });
+    try {
+        const existingRole = guild.roles.cache.find((role) => role.name === roleName);
+        if (existingRole) {
+            return existingRole;
+        } else {
+            return await guild.roles.create({
+                name: roleName,
+                color: color,
+            });
+        }
+    } catch (error) {
+        console.error(error);
+        throw new Error('Failed to create or fetch role');
     }
 }
